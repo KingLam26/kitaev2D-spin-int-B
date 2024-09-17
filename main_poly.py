2a2d62ba13a41e65ef8039cff9433fc4d64d250c
King Lam <kinglam@Kings-MacBook-Pro.local>
2024-09-17 16:46:03 +0100

    proof 1

--- main_poly.py
+++ main_poly.py
@@ -1,6 +1,10 @@
-##### import business #####
-import sympy as sp
-import string, sys, itertools
+from utils_poly import *
+
+diff_print = False
+poly_print_A, poly_print_B = True, True
+quo_A_print, quo_B_print = True, True
+factor_A_print, factor_B_print = True, True
+
 
 ##### run Python read in params #####
 if len(sys.argv) > 1:
@@ -10,244 +14,72 @@ else:
     print("insufficient input provided")
 
 
-def gen_poly(spin_S):
-
-    """
-    Generates two polynomials:
-        1. Poly A represents the sum of all coefficients of all possible diagrams
-           based on the input parameter spin_S under the no-offset assumption.
-        2. Poly B represents the remainder polynomial that needs to be added to Poly A
-           to ensure the correct overall polynomial is obtained when plus-offset is switched on.
-
-    Parameters:
-    spin_S (int): An integer representing the spin value.
-
-    Returns: [for both Poly A and Poly B]
-    sympy.Poly: A SymPy polynomial object representing the polynomial with coefficients 
-                and powers derived from the constructed diagrams and their interactions.
-
-    Description:
-    1.  Construct a dictionary of coefficients for different power values based on spin_S.
-        - `key_list` contains even numbers between 0 and 2*spin_S.
-        - `value_list` contains coefficients, starting with 1 followed by symbolic coefficients.
-
-    2.  Construct a dictionary (`mother_dict`) where keys are tuples representing all 
-        possible combinations of values from `key_list` and values are the products of 
-        corresponding coefficients from `coeff_dict`.
-
-    3.  Constructs daughter diagrams from each mother diagram.
-        - Each mother diagram generates up to four possible daughter diagrams by permuting 
-          the positions of the labels.
-        - `daughter_dict` contains these permutations as keys and their respective 
-          coefficients (or negative coefficients) as values.
-
-    4.  Constructs a dictionary (`polynomial_dict`) where:
-        - Keys are powers of x derived from the sum of labels in the daughter diagrams.
-        - Values are the accumulated coefficients for these powers.
-
-    5.  Constructs the final polynomial expression using SymPy.
-        - The polynomial is created by summing terms of the form x^power * coeff.
-
-    6.  Based on Poly A, Poly B is constructed by extracting coefficients from Poly A involving more 
-        than one algebraic coefficient in {a,b,c,d...}, e.g., a**2 or a*b but not a solo a, b etc.
-
-    Example:
-    >>> gen_polynomial(2)
-    Poly A: Poly(x**4 - 2*a*x**3 + (a**2 + 2)*x**2 - 2*a*x + 1, x, domain='ZZ[a]')
-    Poly B: 
-    """
-
-    spin_2S = spin_S * 2
-
-    # create dict where key list is even numbers between 0 and spin_S
-    # and value list is the coefficents 1, a, b, c, ...
-    key_list = [spin_S*2 - i for i in range(0, spin_S+1, 2)]
-    if len(key_list) > 2:
-        value_list = list(sp.symbols(' '.join(string.ascii_lowercase[:len(key_list)-1])))
-    else:
-        value_list = [sp.symbols('a')]
-    value_list.insert(0, 1)
-    coeff_dict = dict(zip(key_list, value_list))
-
-    # mother diagram dict: construct unique diagrams
-    key_cartesian_product = list(itertools.product(key_list, key_list))
-    mother_dict = {}
-    for pair in key_cartesian_product:
-        mother_dict[pair] = coeff_dict[pair[0]] * coeff_dict[pair[1]]
-
-    # daughter diagram dict: each mother diagram generates {1,2,4} daughter diagrams
-    # key are tuples (i,j,k,l): i,j are top labels y,x and k,l are bottom labels x,y
-    daughter_dict = {}
-    for pair, coeff in mother_dict.items():
-        i,j,k,l = pair[0], spin_2S - pair[0], pair[1], spin_2S - pair[1]
-        quad_permutes = list(set([(i,j,k,l), (j,i,k,l), (i,j,l,k), (j,i,l,k)]))        
-        for quad_permute in quad_permutes:
-            if (quad_permute[0] + quad_permute[2]) % 4 == 0:
-                daughter_dict[quad_permute] = coeff
-            else:
-                daughter_dict[quad_permute] = -coeff
-
-    # construct poly A dict: key is power of z, value is coeff
-    # we transformed x = yd and z = d**2
-    poly_A_dict = {}
-    for quad_permute, coeff in daughter_dict.items():
-        power = int((quad_permute[0] + quad_permute[2]) / 2)
-        if power in poly_A_dict:
-            poly_A_dict[power] += coeff
-        else:
-            poly_A_dict[power] = coeff
-    poly_A_dict = dict(sorted(poly_A_dict.items(), reverse=True))
-
-    # construct poly B dict
-    poly_B_dict = gen_remainder_poly(value_list, poly_A_dict)
-
-    # construct the polynomial in sympy: poly A and poly B
-    poly_A, poly_B, x = 0, 0, sp.symbols('x')
-    for power, coeff in poly_A_dict.items():
-        poly_A += x**power * coeff
-
-    for power, coeff in poly_B_dict.items():
-        poly_B += x**power * coeff
-
-    return sp.Poly(poly_A, x), sp.Poly(poly_B, x)
-
-def gen_remainder_poly(value_list, poly_A_dict):
-    
-    remainder_dict = {}
-    for key, coeff in poly_A_dict.items():
-        if coeff == 1: continue
-        for term in coeff.as_ordered_terms():
-            if "-" in str(term):
-                if -term not in value_list and term / -2 not in value_list:
-                    if key in remainder_dict:
-                        remainder_dict[key] += term
-                    else:
-                        remainder_dict[key] = term
-            else:
-                if term not in value_list and term / 2 not in value_list:
-                    if key in remainder_dict:
-                        remainder_dict[key] += term
-                    else:
-                        remainder_dict[key] = term
-
-
-    return remainder_dict
-
-def diff_poly(polynomial):
-    
-    # differentiate the polynomial
-    derivative = sp.diff(polynomial, sp.symbols("x"))
-    
-    return derivative
-
-def factorize_poly(polynomial):
-    """
-    Factorizes a SymPy polynomial and prints the factors.
-
-    Parameters:
-    polynomial (sympy.Poly or sympy.Expr): A SymPy polynomial object or expression to be factorized.
-
-    Returns:
-    sympy.Expr: The factored polynomial expression.
-
-    Description:
-    - Uses SymPy's `factor` function to factorize the given polynomial.
-    - Prints the factored form of the polynomial.
-    - Returns the factored polynomial for further use.
-
-    Example:
-    >>> x = sp.symbols('x')
-    >>> poly = sp.Poly(x**4 - 2*x**3 + x**2 - 2*x + 1, x)
-    >>> factorized_poly = factorize_polynomial(poly)
-    (x - 1)**2 * (x**2 + 1)
-    """
-    # Ensure the polynomial is in the form of a SymPy expression
-    if isinstance(polynomial, sp.Poly):
-        polynomial = polynomial.as_expr()
-    
-    # Factorize the polynomial
-    factored_poly = sp.factor(polynomial)
-    
-    return factored_poly
-
-def gen_poly_dict(polynomial):
-    """
-    Prints the terms and coefficients of a SymPy polynomial and returns a dictionary of these terms.
-
-    Parameters:
-    polynomial (sympy.Poly): A SymPy polynomial object to be printed and analyzed.
-
-    Returns:
-    dict: A dictionary where keys are the powers of the polynomial and values are the corresponding coefficients.
-
-    Description:
-    - Extracts terms and coefficients from the given polynomial.
-    - Creates a dictionary with the power of the term as the key and the coefficient as the value.
-    - Prints each power and its associated coefficient.
-    - Returns the dictionary for further use.
-
-    Example:
-    >>> poly = Poly(x**4 - 2*a*x**3 + (a**2 + 2)*x**2 - 2*a*x + 1, x, domain='ZZ[a]')
-    """
-
-    terms = polynomial.terms()
-    coeff_dict = {term[0][0]: term[1] for term in terms}
-    return coeff_dict
-
-def print_poly_dict(poly_dict):
-    for key, value in poly_dict.items():
-        print(f"{key}: {value}")
-
-
-##### computation #####
-
+##### main analysis #####
 # Generate the polynomials: poly A and poly B
 poly_A, poly_B = gen_poly(spin_S)
-dict_poly_A, dict_poly_B = gen_poly_dict(poly_A), gen_poly_dict(poly_B)
 
-"""
-# differentiate
+if poly_print_A:
+    print(f"\nPoly A: \n")
+    print_poly_dict(gen_poly_dict(poly_A))
+    
+if poly_print_B:
+    print(f"\nPoly B: \n")
+    print_poly_dict(gen_poly_dict(poly_B))
+
+# Differentiate Poly A
 first_diff_poly_A = diff_poly(poly_A)
 second_diff_poly_A = diff_poly(first_diff_poly_A)
 
 first_diff_dict_poly_A = gen_poly_dict(first_diff_poly_A)
 second_diff_dict_poly_A = gen_poly_dict(second_diff_poly_A)
 
-print_poly_dict(dict_poly_A)
-print_poly_dict(first_diff_dict_poly_A)
-print_poly_dict(second_diff_dict_poly_A)
-"""
+if diff_print:
+    print(f"\nPoly A first diff: \n")
+    print_poly_dict(first_diff_dict_poly_A)
+    print(f"\nPoly A second diff: \n")
+    print_poly_dict(second_diff_dict_poly_A)
 
-# divide by (x-1) if S is odd
+# Divide Poly A by (x-1)**2 if S is odd, else no change
 x = sp.symbols('x')
 if spin_S % 2 != 0:
-    denom = x**2 + 1 - 2*x
-    quo_poly_A, _ = sp.div(poly_A.as_expr(), denom, domain = "QQ")
-    quo_poly_A = sp.Poly(quo_poly_A, x)
+    denom_A = (x-1)**2
 else:
-    quo_poly_A = poly_A
+    denom_A = 1
+quo_poly_A, remain_poly_A = sp.div(poly_A.as_expr(), denom_A, domain = "QQ")
+quo_poly_A = sp.Poly(quo_poly_A, x)
+if remain_poly_A != 0:
+    print("remainder!")
+    quit()
 
-print_poly_dict(gen_poly_dict(poly_A))
-print()
-print_poly_dict(gen_poly_dict(quo_poly_A))
+if quo_A_print:
+    print(f"\nPoly A quotient: \n")
+    print_poly_dict(gen_poly_dict(quo_poly_A))
 
-# factorize
-factor_poly_A = factorize_poly(quo_poly_A)
+# Divide Poly B by x**2 always, and (x-1)**2 if S odd
+if spin_S % 2 != 0:
+    denom_B = x**2 * (x-1)**2
+else:
+    denom_B = x**2
+quo_poly_B, remain_poly_B = sp.div(poly_B.as_expr(), denom_B, domain = "QQ")
+quo_poly_B = sp.Poly(quo_poly_B, x)
+if remain_poly_B != 0:
+    print("remainder!")
+    quit()
 
+if quo_B_print:
+    print(f"\nPoly B quotient: \n")
+    print_poly_dict(gen_poly_dict(quo_poly_B))
 
-factors_list = sp.factor_list(quo_poly_A)
-print()
-print_poly_dict(gen_poly_dict(factors_list[1][0][0]))
+# factorize Poly A
+factors_A_list = sp.factor_list(quo_poly_A)
+if factor_A_print:
+    print(f"\nPoly A factor: \n")
+    print_poly_dict(gen_poly_dict(factors_A_list[1][0][0]))
+
+# factorize Poly B
+factors_B_list = sp.factor_list(quo_poly_B)
+if factor_B_print:
+    print(f"\nPoly B factor: \n")
+    print_poly_dict(gen_poly_dict(factors_B_list[1][0][0]))
+
 quit()
-
-# Extract and print the repeated factor
-for factor, exponent in factors[1]:
-    if exponent == 2:
-        print("Repeated factor:", factor**exponent)
-
-# Printing
-sp.pprint(factor_poly_A, use_unicode=True)
-"""
-sp.pprint(poly_B, use_unicode=True)
-sp.pprint(factor_poly_B, use_unicode=True)
-"""
